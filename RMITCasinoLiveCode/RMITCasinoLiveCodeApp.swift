4e136cbb88c40df80139fb9a292df5aaebd4a39e
TomHuynhSG <tomhuynhsg@gmail.com>
2025-08-05 14:48:23 +0700

    Initial Commit

---
+++ RMITCasinoLiveCode/RMITCasinoLiveCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMITCasinoLiveCodeApp.swift
+//  RMITCasinoLiveCode
+//
+//  Created by Tom Huynh on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RMITCasinoLiveCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
